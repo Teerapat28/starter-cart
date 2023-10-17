66a05ce7d0b81bd079ac2e6b67e182e240778f31
유애나 <77397206+Teerapat28@users.noreply.github.com>
2023-10-17 21:51:17 +0700

    Data Structure Options Map Converting fatch data reducer

---
+++ README.md
@@ -0,0 +1,3 @@
+## Figma URL
+
+[Cart](https://www.figma.com/file/5AwKjnWuM6BhRYmxdQFpky/Cart?node-id=0%3A1&t=lfaO4zazTd7nqF1q-1)
---
+++ src/context.jsx
@@ -0,0 +1,75 @@
+import { useContext, useReducer, useEffect, createContext } from "react";
+import cartItems from "./data";
+import reducer from "./reducer";
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREASE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "./action";
+import { getTotals } from "./utils";
+
+const url = 'https://www.course-api.com/react-useReducer-cart-project';
+
+const AppContext = createContext();
+
+const initialState = {
+  loading: false,
+  // api fatch
+  cart: [],
+  /* local data */ 
+  // cart: new Map(cartItems.map((item) => [item.id, item])),
+};
+
+export const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { totalAmount, totalCost } = getTotals(state.cart);
+
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  };
+
+  const remove = (id) => {
+    dispatch({ type: REMOVE, payload: { id } });
+  };
+
+  const increase = (id) => {
+    dispatch({ type: INCREASE, payload: { id } });
+  };
+
+  const decrease = (id) => {
+    dispatch({ type: DECREASE, payload: { id } });
+  };
+  const fetchData = async() => {
+    dispatch({ type: LOADING })
+    const response = await fetch(url)
+    const cart = await response.json();
+    dispatch({ type: DISPLAY_ITEMS, payload: { cart } });
+  }
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+        clearCart,
+        remove,
+        increase,
+        decrease,
+        totalAmount,
+        totalCost,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
